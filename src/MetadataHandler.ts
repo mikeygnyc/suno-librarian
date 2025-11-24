87f12398cbf69f1a92ad3593feea101ee607e08f
Mike Gales <github@galesnoc.com>
2025-11-24 02:17:29 -0600

    fix for flac comments with blank lines

--- src/MetadataHandler.ts
+++ src/MetadataHandler.ts
@@ -174,7 +174,7 @@ class MetadataHandler {
       `${metadata.clipId}_vorbis.txt`
     );
 
-    fs.writeFileSync(tmpFilePath, lines.join(os.EOL));
+    fs.writeFileSync(tmpFilePath,this.removeBlankLinesFromArray(lines));
     let embeddedImage: boolean = false;
     let imagePath: string = "";
     if (AppConfig.embedImagesInConvertedFiles) {
@@ -213,6 +213,16 @@ class MetadataHandler {
     await execFileAsync("metaflac", metadataArgs);
     fs.rmSync(tmpFilePath);
     console.log(`      <- Done embedding metadata into ${flacPath}`);
+  }
+  private removeBlankLinesFromArray(lines: string[]): string {
+    try {
+      const filteredLines = lines.filter((line) => line.trim() !== "");
+      const newContent = filteredLines.join(lines.join(os.EOL));
+      return newContent
+    } catch (error) {
+      console.error(`Error cleaning comments ${lines}:`, error);
+    }
+    return "";
   }
   private async embedMetadataInAlac(metadata: ISongData) {
     const alacPath = `${AppConfig.alacDirectoryPath}/${metadata.clipId}.m4a`;
